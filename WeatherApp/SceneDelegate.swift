4e67b849cd676e0c1d2f8dbfd60c7d5902ce700f
saengle <zohv817@gmail.com>
2024-07-10 22:39:43 +0900

    [Feat] MainVC Toolbar UI

--- WeatherApp/SceneDelegate.swift
+++ WeatherApp/SceneDelegate.swift
@@ -18,8 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         let vc = MainViewController()
+        let nav = UINavigationController(rootViewController: vc)
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = vc
+        window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
 
--- WeatherApp/UI/Main/MainViewController.swift
+++ WeatherApp/UI/Main/MainViewController.swift
@@ -11,14 +11,45 @@ class MainViewController:  UIViewController {
     
     let mainView = MainView()
     
+    lazy var newReminder = {
+        let item = UIBarButtonItem(image: UIImage(systemName: "map"), style: .plain, target: self, action: #selector(tabbarButtonClicked(_:)))
+        item.tag = 0
+        item.tintColor = .black
+        return item
+    }()
+    lazy var flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+    lazy var addList = {
+        let item = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .plain, target: self, action: #selector(tabbarButtonClicked(_:)))
+        item.tag = 1
+        item.tintColor = .black
+        return item
+    }()
+    
     override func loadView() { view = mainView}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureNav()
     }
-
-
+    
+   
 }
+extension MainViewController {
+    func configureVC() {
+        
+    }
+    private func configureNav() {
+        navigationController?.isNavigationBarHidden = true
+        navigationController?.isToolbarHidden = false
+       var items = [UIBarButtonItem]()
 
+       [newReminder, flexibleSpace, addList].forEach {
+           items.append($0)
+       }
+
+       self.toolbarItems = items
+    }
+    
+    @objc private func tabbarButtonClicked(_ sender: UIBarButtonItem) {}
+}
 
