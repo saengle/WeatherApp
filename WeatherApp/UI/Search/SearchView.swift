0091d29df51ed142f5081976061ed39841109837
saengle <zohv817@gmail.com>
2024-07-11 09:05:27 +0900

    [Add] SearchScreen View, VC files

---
+++ WeatherApp/UI/Search/SearchView.swift
@@ -0,0 +1,19 @@
+//
+//  SearchView.swift
+//  WeatherApp
+//
+//  Created by 쌩 on 7/11/24.
+//
+
+import UIKit
+
+class SearchView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ WeatherApp/UI/Search/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  WeatherApp
+//
+//  Created by 쌩 on 7/11/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
