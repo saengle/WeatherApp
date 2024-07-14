c1f6a5a2d18b12018476af26df48263831bd1dc5
saengle <zohv817@gmail.com>
2024-07-14 21:35:47 +0900

    [Feat] CityManager
    Feat. CityModel , Manager

--- WeatherApp/Resource/CityDataManager.swift
+++ WeatherApp/Resource/CityDataManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+final class CityDataManager {
+    
+    static let shared = CityDataManager()
+    
+    private init() {}
+    
+    func fetchData() -> [City] {
+        if let path = Bundle.main.path(forResource: "CityList", ofType: "json")  {
+            if let jsonString = try? String(contentsOfFile: path){
+                let decoder = JSONDecoder()
+                let data = jsonString.data(using: .utf8)
+                if let data = data,
+                   let city = try? decoder.decode([City].self, from: data) {
+                    return city
+                }
+            }
+        }
+        return []
+    }
+}
--- WeatherApp/Resource/Model/City.swift
+++ WeatherApp/Resource/Model/City.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+// MARK: - City
+struct City: Codable {
+    let id: Int
+    let name: String
+    let state: State
+    let country: Country
+    let coord: Coord
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+}
+
+enum Country: String, Codable {
+    case hk = "HK"
+    case jp = "JP"
+    case kr = "KR"
+    case us = "US"
+}
+
+enum State: String, Codable {
+    case ca = "CA"
+    case empty = ""
+    case tx = "TX"
+}
+
+typealias Cities = [City]
