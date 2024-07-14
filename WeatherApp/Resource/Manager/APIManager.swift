5968e5dfd6d3ad8a6bfcb8b37ab900c7588503a6
saengle <zohv817@gmail.com>
2024-07-15 04:49:41 +0900

    [Feat] APIManager

--- WeatherApp/Resource/Manager/APIManager.swift
+++ WeatherApp/Resource/Manager/APIManager.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+import Alamofire
+
+class APIManager {
+    
+    static func fetchAPI<T: Decodable>(api: APIModel, type: T.Type = T.self, completion: @escaping((Result<T, AFError>) -> Void)) {
+        AF.request(api.endpoint, method: api.method, parameters: api.parameter, encoding: api.encoding).responseDecodable(of: T.self) { response in
+            switch response.result {
+            case .success(let repositories):
+                completion(.success(repositories))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
