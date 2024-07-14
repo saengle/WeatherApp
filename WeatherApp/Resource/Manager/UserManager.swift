379d01fbf24cd6cbd51bb1a17ca4b38c201dc0c5
saengle <zohv817@gmail.com>
2024-07-15 04:49:54 +0900

    [Feat] UserManager

--- WeatherApp/Resource/Manager/UserManager.swift
+++ WeatherApp/Resource/Manager/UserManager.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct UserManager {
+    
+    static var lat: Double {
+        get { UserDefaults.standard.double(forKey: "lat") }
+        set { UserDefaults.standard.set(newValue, forKey: "lat") }
+    }
+    static var lon: Double {
+        get { UserDefaults.standard.double(forKey: "lon") }
+        set { UserDefaults.standard.set(newValue, forKey: "lon") }
+    }
+}
